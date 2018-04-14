24ee4116cd6873b1ad555ca8f852dbe4e50b8f8e
chemzqm <chemzqm@gmail.com>
2018-04-15 03:53:02 +0800

    Release 1.0.2

---
+++ publish.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+json -e 'var v = this.version; var n = parseInt(v.match(/\.(\d+)$/)[1]); n++ ; this.version = v.replace(/\.\d+$/, "." + n)' -f package.json -I
+version=`cat package.json | json version`
+echo $version
+
+cat << EOF | sed "s+version+$version+" > wxdash.xml
+<entry>
+    <version>version</version>
+    <url>https://raw.githubusercontent.com/chemzqm/wx-dash/master/wxapp.tgz</url>
+</entry>
+EOF
+git add .
+tar --exclude='.DS_Store' -cvzf wxapp.tgz wxapp.docset
+git commit -a -m "Release $version"
+git tag -a "$version" -m "Release $version"
+git push --tags
+git push
